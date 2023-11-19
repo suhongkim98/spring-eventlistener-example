07204296af5256318740aa8d338a6ad9c72867bc
suhongkim98 <kshz9815@naver.com>
2023-11-19 15:17:59 +0900

    Order 순서 지정 예제, 비동기 처리 예제 추가

--- src/main/java/com/example/eventlistener/mailsender/MailSenderEventListener.java
+++ src/main/java/com/example/eventlistener/mailsender/MailSenderEventListener.java
@@ -2,6 +2,8 @@ package com.example.eventlistener.mailsender;
 
 import com.example.eventlistener.order.OrderEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Propagation;
@@ -12,13 +14,13 @@ import org.springframework.transaction.event.TransactionalEventListener;
 /**
  * 1. @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
  * default 값이며, 트랜잭션이 commit 되었을 때 이벤트를 실행합니다.
- *
+ * <p>
  * 2. @TransactionalEventListener(phase = TransactionPhase.ROLLBACK)
  * 트랜잭션이 rollback 되었을 때 이벤트를 실행합니다.
- *
+ * <p>
  * 3. @TransactionalEventListener(phase = TransactionPhase.AFTER_COMPLETION)
  * 트랜잭션이 completion(commit 또는 rollback) 되었을 때 이벤트 실행합니다.
- *
+ * <p>
  * 4. @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
  * 트랜잭션이 commit 되기 전에 이벤트를 실행합니다.
  */
@@ -34,6 +36,16 @@ public class MailSenderEventListener {
     @EventListener
     public void defaultEventListener(OrderEvent orderEvent) {
         System.out.println("MailSenderEventListener[default] 트랜잭션 성공 여부 상관 없이 메일을 보냅니다. : " + orderEvent);
+    }
+
+    /**
+     * Order 로 이벤트 리스너 간 호출 순서 지정 가능, 숫자가 작을 수록 우선된다.
+     * 지정하지 않을 때 디폴트는 Ordered.LOWEST_PRECEDENCE
+     */
+    @Order(value = Ordered.HIGHEST_PRECEDENCE)
+    @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
+    public void transactionalEventListenerBeforeCommitOrdered(OrderEvent event) {
+        System.out.println("MailSenderEventListener TransactionPhase.BEFORE_COMMIT(Ordered.HIGHEST_PRECEDENCE) ---> " + event);
     }
 
     @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
@@ -64,5 +76,15 @@ public class MailSenderEventListener {
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void transactionalEventListenerAfterCommitRequiredNew(OrderEvent event) {
         System.out.println("MailSenderEventListener TransactionPhase.AFTER_COMMIT(REQUIRES_NEW) ---> " + event);
+    }
+
+    /**
+     * 트랜잭션이 성공한 이후 이벤트 리스너에서 DB에 쓰기작업 등을 추가로 해야하는 경우
+     * 혹은 @Async 로 별도의 스레드에서 처리하도록 함
+     */
+    @Async
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void transactionalEventListenerAfterCommitAsync(OrderEvent event) {
+        System.out.println("MailSenderEventListener TransactionPhase.AFTER_COMMIT(Async) ---> " + event);
     }
 }
